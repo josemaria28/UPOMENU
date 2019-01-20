04a10f3d0cf1158b2df2e0da49909aca5e836e32
Daniemv <46506970+Daniemv@users.noreply.github.com>
2019-01-20 19:05:32 +0100

    cambiado forma nueva

--- js/objetos.js
+++ js/objetos.js
@@ -1,8 +1,67 @@
-// Objetos
-function UpoMenu(){
-	this.platos = new Array();
-	this.ingredientes = new Array();
+//Clase principal del proyecto
+class UpoMenu{
+    constructor()
+    {
+        this.listaClientes = [];
+    }
+    // Añadir un plato
+	añadirPlato(oPlato){
+	var pAñadido = false;
+	if(this._buscaPlato(oPlato.nombre) == null){
+		this.platos.push(oPlato);
+		pAñadido = true;
+	}
+	return pAñadido;
+	}
+
+	// Buscar platos
+	buscarPlato(oPlato){
+	var bPlato = null;
+	for (var i = 0; i < this.platos.length && bPlato == null; i++) {
+		if (this.platos[i].nombre == oPlato) {
+		bPlato = this.platos[i];
+		}
+	}
+	return bPlato;
+	}	
+    _buscarCliente(sDni)
+    {
+        var oCliente = null;
+        var i=0;
+        while(i<this.listaClientes.length && oCliente == null)
+        {
+            if(this.listaClientes[i].dni == sDni)
+            {
+                oCliente = this.listaClientes[i].dni;
+            }
+            i++;
+        }
+        return oCliente;
+    }
+    altaCliente(oCliente)
+    {
+        var bInsertado = false;
+
+        if(this._buscarCliente(oCliente.dni)==null)
+        {
+            this.listaClientes.push(oCliente);
+            bInsertado = true;
+        }
+        return bInsertado;
+    }
 }
+
+//Cliente, necesita nombre,teléfono,email y un número identificador(suponemos que es el dni)
+class Cliente{
+    constructor(sNombre,iTelefono,sEmail,sDni)
+    {
+        this.nombre = sNombre;
+        this.telefono = parseInt(iTelefono);
+        this.email = sEmail;
+        this.dni = sDni;
+    }
+}
+
 // Platos
 class Plato{
 	constructor(iId,sNombre,sTipo,fPrecio){
@@ -23,29 +82,3 @@ class Ingrediente extends Plato{
 	}
 	
 }
-
-
-
-// Funciones y Operaciones
-
-// Añadir un plato
-UpoMenu.prototype.añadirPlato = function(oPlato){
-	var pAñadido = false;
-	if(this._buscaPlato(oPlato.nombre) == null){
-		this.platos.push(oPlato);
-		pAñadido = true;
-	}
-	return pAñadido;
-};
-
-// Buscar platos
-UpoMenu.prototype._buscarPlato = function(oPlato){
-	var bPlato = null;
-	for (var i = 0; i < this.platos.length && bPlato == null; i++) {
-		if (this.platos[i].nombre == oPlato) {
-		bPlato = this.platos[i];
-		}
-	}
-	return bPlato;
-};
-
