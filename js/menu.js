71ae3d5dbd9efeab3a79d17831ce9fdeae3f2886
ilde123 <45488123+ilde123@users.noreply.github.com>
2019-01-25 12:07:23 +0100

    ok

--- js/menu.js
+++ js/menu.js
@@ -4,100 +4,21 @@ var upoMenu = new UpoMenu();
 //var xml = cargarXML("https://github.com/ilde123/UPOMENU/blob/master/XML/menu.xml");
 var xml = cargarXML("../XML/menu.xml");
 
-// Eventos
-
 function inicio() {
-//	datosPrueba();
-//	actualizarDesplegable();
+	datosPrueba();
+	actualizarDesplegable();
 	inicializarEventos();
 }
 
-// Todo lo demas
-function validarFormulario() {
-	limpiarErrores();
-
-	var valido = true;
-	var error = new Array();
-
-	var nombre = document.querySelector("#txtNombreEvento").value.trim();
-	var exReg = /^[a-zA-ZÁÉÍÓÚñáéíóúÑ0-9]{6,50}$/;
-
-	if (exReg.test(nombre) == false) {
-		valido = false;
-
-		document.querySelector("#txtNombreEvento").classList.add("is-invalid");
-		error.push("El nombre debe contener caracteres alfanumérico de entre 6 y 50 caracteres");
-	}
-
-	var fecha = document.querySelector("#txtFecha").value;
-//	exreg = /^[0-9]{4}([\-/.])(0?[1-9]|1[1-2])([\-/.])(3[01]|[12][0-9]|0?[1-9])$/;
-
-	if (fecha == "") {
-		valido = false;
-
-		document.querySelector("#txtFecha").classList.add("is-invalid");
-		
-		error.push("Debe seleccionar una fecha");
-	}
-
-	var comensales = document.querySelector("#txtComensales").value.trim();
-	exReg = /^[0-9]{1,3}$/;
-
-	if (!exReg.test(comensales)) {
-		valido = false;
-
-		document.querySelector("#txtComensales").classList.add("is-invalid");
-		error.push("Comensales debe ser un número");
-	}
-
-	var duracion = document.querySelector("#txtDuracion").value.trim();
-	exReg = /(\W|^)([mM]añana|[tT]arde|[nN]oche)(\W|$)/;
-
-	if (!exReg.test(duracion)) {
-		valido = false;
-
-		document.querySelector("#txtDuracion").classList.add("is-invalid");
-		error.push("Debe escribir Mañana, Tarde o Noche");
-	}
-
-	if (!valido) {
-		document.querySelector(".is-invalid").focus();
-		mostrarMensajeError(error);
-	}
-	else {
-//		var evento = new Evento(nombre, fecha, comensales, duracion)
-//		upoMenu.agregarEvento();
-
-		frmEvento.submit();
+function inicializarEventos() {
+	var desplegables = document.querySelectorAll("select");
+	
+	for (let i = 0; i < desplegables.length; i++) {
+		desplegables[i].addEventListener("change", actualizarPrecio);
 	}
 }
 
-function mostrarMensajeError(error) {
-	var elemento = document.querySelectorAll(".is-invalid");
-
-	for (var i = 0; i < elemento.length; i++) {
-		var div = document.createElement("div");
-		div.classList.add("texto-error");
-		div.textContent = error[i];
-
-		elemento[i].parentElement.appendChild(div);
-	}
-}
-
-function limpiarErrores() {
-	var errores = document.querySelectorAll(".is-invalid");
-
-	for (var i = 0; i < errores.length; i++) {
-		errores[i].classList.remove("is-invalid");
-	}
-
-	errores = document.querySelectorAll(".texto-error");
-
-	for (var i = 0; i < errores.length; i++) {
-		errores[i].remove();
-	}
-}
-
+// Actualiza los options del select
 function actualizarDesplegable() {
 	var primerPlato = document.querySelector("#txtPrimerPlato");
 	var platos = upoMenu.platos;
@@ -148,6 +69,7 @@ function actualizarDesplegable() {
 	actualizarPrecio();
 }
 
+// Actualiza el input type="text" con el precio del plato cuando cambia el option
 function actualizarPrecio() {
 	var desplegables = document.querySelectorAll("select");
 	var precioTotal = 0;
@@ -166,6 +88,7 @@ function actualizarPrecio() {
 	total.value = precioTotal.toFixed(2) + " €";
 }
 
+// Agrega un input type="text" con el precio del plato
 function mostrarPrecio(elemento) {
 	var div = document.createElement("div");
 	div.classList.add("col-md-1", "capa-precio");
@@ -179,6 +102,7 @@ function mostrarPrecio(elemento) {
 	padre.after(div);
 }
 
+// Borra el input type="text" que contiene el precio del plato
 function borrarPrecio() {
 	var capas = document.querySelectorAll(".capa-precio");
 
@@ -189,39 +113,7 @@ function borrarPrecio() {
 	}
 }
 
-function inicializarEventos() {
-	document.querySelector("#btnAceptarEvento").addEventListener("click", validarFormulario);
-	document.querySelector("#btnIncremento").addEventListener("click", actualizaValor);
-	document.querySelector("#btnDecremento").addEventListener("click", actualizaValor);
-	var desplegables = document.querySelectorAll("select");
-	
-	for (let i = 0; i < desplegables.length; i++) {
-		desplegables[i].addEventListener("change", actualizarPrecio);
-	}
-}
-
-function actualizaValor(e) {
-
-	var comensales = document.querySelector("#txtComensales");
-	var valor = parseInt(comensales.value);
-
-	if (valor >= 0) {
-		if (e.target.id == "btnIncremento") {
-			valor++;
-
-			comensales.value = valor;
-		}
-		else if (valor > 0) {
-			valor--;
-
-			comensales.value = valor;
-		}
-	}
-}
-
-
-
-
+// Da formato de float a un numero
 function numeroComa(numero) {
 	numero.toString();
 	numero = numero.replace(",", ".");
