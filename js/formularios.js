8e205b7d0042b2c1daccebf99caaa7c9b15d64fa
Dani <daniiemv98@gmail.com>
2019-01-29 20:43:24 +0100

    cambios Index

---
+++ js/formularios.js
@@ -0,0 +1,11 @@
+window.addEventListener("load",inicioIndex,false);
+
+function inicioIndex()
+{
+    ocultarFormularios();
+}
+
+function ocultarFormularios()
+{
+    document.getElementById("frmMenu").style.display="none";
+}
