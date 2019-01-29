53703f28a257d5df2c45edd555383f8523b5eb20
Dani <daniiemv98@gmail.com>
2019-01-29 20:47:44 +0100

    Update formularios.js

--- js/formularios.js
+++ js/formularios.js
@@ -3,9 +3,17 @@ window.addEventListener("load",inicioIndex,false);
 function inicioIndex()
 {
     ocultarFormularios();
+    document.getElementById("btnMenu").addEventListener("click",verAltaMenu);
 }
 
 function ocultarFormularios()
 {
     document.getElementById("frmMenu").style.display="none";
+}
+
+function verAltaMenu()
+{
+    ocultarFormularios();
+    document.getElementById("frmMenu").style.display="block";
+    frmMenu.reset();
 }
