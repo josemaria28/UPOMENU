f002a5457731917e2695adfc4b8b1413021a632b
Dani <daniiemv98@gmail.com>
2019-01-31 02:10:27 +0100

    Update formularios.js

--- js/formularios.js
+++ js/formularios.js
@@ -509,6 +509,7 @@ function verListadoClientes()
         oCelda = oFila.insertCell(-1);
         oCelda.textContent = listaClientes[i].password;
         oFila = oTBody.insertRow(-1);
+        oCelda = oFila.insertCell(-1);
     }
     
     divListado.appendChild(oTabla);
