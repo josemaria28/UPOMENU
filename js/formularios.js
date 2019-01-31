fbd8a96c78a3c8572880882052a7094d1ed24e67
Dani <daniiemv98@gmail.com>
2019-01-31 03:25:09 +0100

    Update formularios.js

--- js/formularios.js
+++ js/formularios.js
@@ -775,7 +775,7 @@ function iniciarSesion()
         formulario.reset();
         document.querySelector("#dropdownMenu1").remove();
         document.querySelector("#liReg").remove();
-        var bienvenida = document.createElement("p");
+        var bienvenida = document.createElement("h3");
         bienvenida.style.color="rgba(151, 5, 5, 0.986)";
         bienvenida.style.textAlign="center";
         var textoBienvenida = document.createTextNode="Bienvenido usuario";
