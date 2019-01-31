b206dc8f192c40a4ffc50b1d04888fb17e4ccaf8
Dani <daniiemv98@gmail.com>
2019-01-31 02:51:01 +0100

    Update formularios.js

--- js/formularios.js
+++ js/formularios.js
@@ -538,7 +538,7 @@ function verListadoClientes()
     borrarTablas();
     var divListado = document.getElementById("listaClientes");
     var encabezado = document.createElement("h2");
-    encabezado.style.alignContent="center";
+    divListado.style.alignContent="center";
     var tituloEncabezado = document.createTextNode("Listado de Clientes de UpoMenu");
     encabezado.appendChild(tituloEncabezado);
     var oTabla = document.createElement("table");
